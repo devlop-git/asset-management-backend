1bc607f466ee94bcd9f882f660c44bbff1d74baf
Swati Prajapati <swati.neosoft@navgrahaa.com>
2025-09-17 15:45:02 +0530

    error resolved

--- src/role_permissions/role_permissions.module.ts
+++ src/role_permissions/role_permissions.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RolePermissionsService } from './role_permissions.service';
 import { RolePermissionsController } from './role_permissions.controller';
+import { Role } from 'src/role/entities/role.entity';
+import { Permission } from 'src/permissions/entities/permissions.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolePermission } from './entities/role_permissions.entity';
+import { PermissionsService } from 'src/permissions/permissions.service';
 
 @Module({
-  providers: [RolePermissionsService],
+  imports:[
+      TypeOrmModule.forFeature([Permission,Role,RolePermission])
+    ],
+  providers: [RolePermissionsService,PermissionsService],
   controllers: [RolePermissionsController]
 })
 export class RolePermissionsModule {}
