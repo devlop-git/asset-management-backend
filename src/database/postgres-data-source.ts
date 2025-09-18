16ba256c4034383bcd76cf7a16e43b746efcde7a
Shilwant Gupta <shilwant.gupta@navgrahaa.com>
2025-09-18 18:01:46 +0530

    fixed changes for permission

--- src/database/postgres-data-source.ts
+++ src/database/postgres-data-source.ts
@@ -9,7 +9,7 @@ export const PostgresDataSource = new DataSource({
   host: configService.get('POSTGRES_HOST'),
   port: Number(configService.get('POSTGRES_PORT')),
   username: configService.get('POSTGRES_USERNAME'),
-  password: configService.get('POSTGRES_PASSWORD'),
+  password: String(configService.get('POSTGRES_PASSWORD')),
   database: configService.get('POSTGRES_DATABASE'),
   
   entities:
